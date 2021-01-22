5771a77cd238fcb0e99222687d3fd3671f842ac8
Binyamin Binni <53377458+Binyamin-binni@users.noreply.github.com>
2021-01-22 05:55:43 +0500

    Update bxi.py

--- bxi.py
+++ bxi.py
@@ -1,8 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/python2
 #coding=utf-8
 import os,platform,time,base64
-if not os.path.isfile('/data/data/com.termux/files/usr/bin/wget'):
-	os.system('pkg update && pkg install wget -y')
 if not os.path.isfile('/data/data/com.termux/files/usr/bin/node'):
 	os.system('pkg update && pkg install nodejs -y')
 bit=platform.architecture()[0]
